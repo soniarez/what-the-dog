29567ba4892c978183f90029830aa38cb6b99dcf
soniarez <sonia.reyes.posada@gmail.com>
2022-11-05 10:20:12 -0300

    populating list of breeds

--- src/components/finderView/SideBar.jsx
+++ src/components/finderView/SideBar.jsx
@@ -10,84 +10,78 @@ import axios from "axios";
 const SideBar = () => {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState(false);
-  const [animal, setAnimal] = useState(null);
+  const [breeds, setBreeds] = useState([]);
 
-  
-useEffect(() => {
-  const getDogs = async () => {
-    try {
-      const dogsFromApi = await fetchDogs();
-      console.log(dogsFromApi, "dogs from api");
-      setAnimal(dogsFromApi);
-    } catch (err) {
-      console.log(err, "error fetching data from API");
+  useEffect(() => {
+    const getDogs = async () => {
+      try {
+        const dogsFromApi = await fetchDogs();
+        dogConverter(dogsFromApi);
+        setBreeds(dogsFromApi.message);
+      } catch (err) {
+        console.log(err, "error fetching data from API");
+      }
+    };
+    getDogs();
+  }, []);
+
+  //CONVERTING OBJECT INTO ARRAY
+  const dogConverter = (rawDogs) => {
+    for (const breed in rawDogs.message) {
+      return rawDogs.message[breed];
     }
-  }
-  getDogs();
-}, []);
+  };
 
   //FETCHING DOG DATA FROM API
-const fetchDogs = async () => {
-  try {
-    let res = await axios.get("https://dog.ceo/api/breeds/list/all");
-    let data = await res.data;
-    return data;
-  } catch (err) {
-    if (err.response) {
-      console.log(err.response.status);
-      console.log(err.response.data)
+  const fetchDogs = async () => {
+    try {
+      let res = await axios.get("https://dog.ceo/api/breeds/list/all");
+      let breedsObject = await res.data;
+      return breedsObject;
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response.status);
+        console.log(err.response.data);
+      }
     }
-  }
-}
+  };
 
   const handleClick = () => {
     setOpen(!open);
   };
 
-  const dogs = [
-    {
-      id: 1,
-      breed: "chihuahua",
-      subBreed: "",
-    },
-    {
-      id: 2,
-      breed: "pug",
-      subBreed: "",
-    },
-    {
-      id: 3,
-      breed: "bulldog",
-      subBreed: ["boston", "english", "french"],
-    },
-  ];
+  //aca estamos sacando una lista de nombres de perro exrtayendo las llaves esto es una lista de string
+  const listOfBreeds = Object.keys(breeds);
 
   return (
     <div>
-      {dogs.map((dog) => (
+      {listOfBreeds.map((dog, index) => (
         <List
-          key={dog.id}
+          key={index}
           sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
           component="nav"
           aria-labelledby="nested-list-subheader"
         >
-          {dog.subBreed.length === 0 ? (
-            <ListItemButton selected={selected} onClick={()=>setSelected(!selected)}>
-              <ListItemText primary={dog.breed} />
+          {breeds[dog].length === 0 ? (
+            <ListItemButton
+              selected={selected}
+              onClick={() => setSelected(!selected)}
+            >
+              <ListItemText primary={dog} />
             </ListItemButton>
           ) : (
             <>
               <ListItemButton onClick={handleClick}>
-                <ListItemText primary={dog.breed} />
+                <ListItemText primary={dog} />
                 {open ? <ExpandLess /> : <ExpandMore />}
               </ListItemButton>
               <Collapse in={open} timeout="auto" unmountOnExit>
-                {dog.subBreed.map((subBreed)=> (
-                  <List key={subBreed.index} component="div" disablePadding>
-                  <ListItemButton sx={{ pl: 4 }}>
-                    <ListItemText primary={subBreed} />
-                  </ListItemButton>
-                </List>
+                {breeds[dog].map((subBreed, _subIndex) => (
+                  <List key={_subIndex} component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 4 }}>
+                      <ListItemText primary={subBreed} />
+                    </ListItemButton>
+                  </List>
                 ))}
               </Collapse>
             </>
