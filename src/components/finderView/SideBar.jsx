93497ee94e8fd7f7469817cb32af13f7a26a968a
soniarez <sonia.reyes.posada@gmail.com>
2022-11-05 00:41:25 -0300

    nested list sidebar

--- src/components/finderView/SideBar.jsx
+++ src/components/finderView/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
@@ -7,39 +7,64 @@ import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 
 const SideBar = () => {
-    const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = useState(false);
 
-    const handleClick = () => {
-      setOpen(!open);
-    };
-  
-    return (
-      <div >
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const dogs = [
+    {
+      id: 1,
+      breed: "chihuahua",
+      subBreed: "",
+    },
+    {
+      id: 2,
+      breed: "pug",
+      subBreed: "",
+    },
+    {
+      id: 3,
+      breed: "bulldog",
+      subBreed: ["boston", "english", "french"],
+    },
+  ];
+
+  return (
+    <div>
+      {dogs.map((dog) => (
         <List
+          key={dog.id}
           sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
           component="nav"
           aria-labelledby="nested-list-subheader"
         >
-          <ListItemButton>
-            <ListItemText primary="Sent mail" />
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemText primary="Drafts" />
-          </ListItemButton>
-          <ListItemButton onClick={handleClick}>
-            <ListItemText primary="Inbox" />
-            {open ? <ExpandLess /> : <ExpandMore />}
-          </ListItemButton>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItemButton sx={{ pl: 4 }}>
-                <ListItemText primary="Starred" />
+          {dog.subBreed.length === 0 ? (
+            <ListItemButton>
+              <ListItemText primary={dog.breed} />
+            </ListItemButton>
+          ) : (
+            <>
+              <ListItemButton onClick={handleClick}>
+                <ListItemText primary={dog.breed} />
+                {open ? <ExpandLess /> : <ExpandMore />}
               </ListItemButton>
-            </List>
-          </Collapse>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                {dog.subBreed.map((subBreed)=> (
+                  <List key={subBreed.index} component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary={subBreed} />
+                  </ListItemButton>
+                </List>
+                ))}
+              </Collapse>
+            </>
+          )}
         </List>
-      </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
